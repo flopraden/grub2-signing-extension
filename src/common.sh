84ec4608dbf94444a74493cdb6cd54bc14861521
Praden Florian <florian.praden@quicksign.com>
2020-04-26 14:29:49 +0200

    * [common] + add utils function from https://natelandau.com/bash-scripting-utilities/ to print beautifuly

--- src/common.sh
+++ src/common.sh
@@ -7,6 +7,39 @@ yellow=$(tput setaf 3)
 normal=$(tput sgr0)
 
 
+##** From: https://natelandau.com/bash-scripting-utilities/
+bold=$(tput bold)
+underline=$(tput sgr 0 1)
+reset=$(tput sgr0)
+
+purple=$(tput setaf 171)
+ared=$(tput setaf 1)
+agreen=$(tput setaf 76)
+tan=$(tput setaf 3)
+blue=$(tput setaf 38)
+
+#
+# Headers and  Logging
+#
+
+e_header() { printf "\n${bold}${purple}==========  %s  ==========${reset}\n" "$@" 
+}
+e_arrow() { printf "➜ $@\n"
+}
+e_success() { printf "${agreen}✔ %s${reset}\n" "$@"
+}
+e_error() { printf "${ared}✖ %s${reset}\n" "$@"
+}
+e_warning() { printf "${tan}➜ %s${reset}\n" "$@"
+}
+e_underline() { printf "${underline}${bold}%s${reset}\n" "$@"
+}
+e_bold() { printf "${bold}%s${reset}\n" "$@"
+}
+e_note() { printf "${underline}${bold}${blue}Note:${reset}  ${blue}%s${reset}\n" "$@"
+         }
+##**##
+
 # Find file to be signed/verified
 # Parameter: none
 # Descr:
